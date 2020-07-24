8d5b57de6c88e85cefd51b635da03dc33b63fc5a
guillenjs <jorgeguilln@gmail.com>
2020-07-24 10:26:29 -0400

    created joiner table and associations

---
+++ db/migrate/20200724142036_create_restaurant_pizzas.rb
@@ -0,0 +1,10 @@
+class CreateRestaurantPizzas < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurant_pizzas do |t|
+      t.references :restaurant, foreign_key: true
+      t.references :pizza, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
