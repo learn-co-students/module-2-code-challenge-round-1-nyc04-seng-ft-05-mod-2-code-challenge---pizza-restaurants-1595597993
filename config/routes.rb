2451e8fcfae0e5b89a94815171323024b3c66e4e
kceron <karemiceron@gmail.com>
2020-07-24 12:14:14 -0400

    my work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get '/restaurants', to: 'restaurants#index'
-  get '/pizzas', to: 'pizzas#index'
+  
+  resources :pizzas
+  resources :restaurants
+  resources :restaurant_pizzas
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/pizzas', to: 'pizzas#index'
 end
