813dbb14245866124c077e837375f9c682bbd602
Fraciscv <Francisco.Andaur@icloud.com>
2020-07-24 12:05:42 -0400

    lost track of tasks, adding buttons to show i know what im doing

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,10 +1,12 @@
 class PizzasController < ApplicationController
+  before_action :find_pizza, only: [:show, :edit]
+
   def index
     @pizzas = Pizza.all
   end
   
   def show
-    @pizza = Pizza.find(params[:id])
+    
   end
 
   def new
@@ -23,9 +25,17 @@ class PizzasController < ApplicationController
 
   end
 
+  def edit
+    render :edit
+  end
+
   private
   def pizza_params
     params.require(:pizza).permit(:name, :ingredients)
+  end
+
+  def find_pizza
+    @pizza = Pizza.find(params[:id])
   end
 
 end
