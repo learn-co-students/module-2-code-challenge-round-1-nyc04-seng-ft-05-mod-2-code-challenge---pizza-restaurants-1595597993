35e7ea16fd6b330fca04ba1520a5ca4dee14b864
Ginger-Mano <mcasimirian@gmail.com>
2020-07-24 12:12:57 -0400

    end

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get '/restaurants', to: 'restaurants#index'
-  get '/pizzas', to: 'pizzas#index'
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/:id', to: 'restaurants#show'
+
+  # get '/pizzas', to: 'pizzas#index'
+  # get '/pizzas/:id', to: 'pizzas#show'
+  # get '/pizzas/new', to: 'pizzas#new'
+  # post '/pizzas'
+
+  resources :restaurants
+  resources :pizzas
+  resources :restaurant_pizzas
+
 end
