b07cc9365cf84e96a8b60e80efeb740d2ea4e83b
Valkryn <calvinraphaelg@gmail.com>
2020-07-24 12:16:36 -0400

    times up

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,34 @@
 class PizzasController < ApplicationController
+
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @restaurants = Restaurant.all
+    @pizza = Pizza.new
+    @errors = flash[:errors]
+  end
+
+  def create
+    pizza = Pizza.create(pizza_params)
+    #if pizza
+      #redirect_to pizza_path(pizza)
+    #else
+    #flash[:errors] = pizza.errors.full_messages
+    #redirect_to pizza_path
+    #end
+    redirect_to pizza_path(pizza)
+  end
+
+
+  private
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients)
+  end
+
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,11 @@
 class Pizza < ApplicationRecord
+  has_many :restaurant_pizzas
+  has_many :restaurants, through: :restaurant_pizzas
+  validates :name , presence: true #{message: "must have a name"}
+  validates :name ,uniqueness: true #{message: "Name already taken"}
+  validates :ingredients , presence: true
+
+
+
+
 end
