a503fe59b4718c56db42915dc23f1f4d6b5a94c4
tracycollins <tc@threeceemedia.com>
2020-07-24 12:13:28 -0400

    working on restaurant show with pizzas

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,54 @@
 class PizzasController < ApplicationController
+
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+
+  def create
+
+    @pizza = Pizza.new(pizza_params)
+
+    respond_to do |format|
+      if @pizza.save
+        format.html { redirect_to @pizza, notice: 'pizza was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @pizza.update(pizza_params)
+        format.html { redirect_to @pizza, notice: 'pizza was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @pizza.destroy
+    respond_to do |format|
+      format.html { redirect_to pizzas_url, notice: 'pizza was successfully destroyed.' }
+    end
+  end
+
+  private
+    def set_pizza
+      @pizza = pizza.find(params[:id])
+    end
+
+    def pizza_params
+      params.require(:pizza).permit(:name, :ingredients)
+    end
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,55 @@
 class RestaurantsController < ApplicationController
+
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    byebug
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'restaurant was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @restaurant.update(restaurant_params)
+        format.html { redirect_to @restaurant, notice: 'restaurant was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    respond_to do |format|
+      format.html { redirect_to restaurants_url, notice: 'restaurant was successfully destroyed.' }
+    end
+  end
+
+  private
+    def set_restaurant
+      @restaurant = restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address)
+    end
+
 end
