20a3e3e62b5a05bd82ce61f7a36f0299dde7e8db
“DonatelloTHM” <donat.pllana@outlook.com>
2020-07-24 12:13:18 -0400

    finished

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,42 @@
 class PizzasController < ApplicationController
+  before_action :find_id, only: [:show,:edit,:update,:destroy]
   def index
     @pizzas = Pizza.all
   end
+
+  def new
+    @pizza=Pizza.new
+    @errors=flash[:errors]
+  end
+
+  def create
+    @pizza=Pizza.create(pizza_params)
+    @pizza.restaurants=(params[:pizza][:restaurants])
+
+    if @pizza.valid?
+      redirect_to pizza_path(@pizza)
+    else
+      flash[:errors]=@pizza.errors.full_messages
+      redirect_to new_pizza_path
+    end
+    
+  end
+
+  def show
+  end
+
+
+
+
+
+  private
+
+  def find_id
+    @pizza=Pizza.find(params[:id])
+  end
+
+  def pizza_params
+    params.require(:pizza).permit(:name,:ingredients)
+  end
+
 end
