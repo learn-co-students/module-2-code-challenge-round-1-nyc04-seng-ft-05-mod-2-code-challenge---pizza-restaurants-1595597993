3e6af4da1b2e20d33524a2ec4d1415ec55af7973
AlveeM <alvee.akand@outlook.com>
2020-07-24 12:10:50 -0400

    done

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,34 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+  
+  def edit
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def create
+    @pizza = Pizza.new(pizza_params)
+    
+    if @pizza.save
+      restaurant_id = params[:pizza][:restaurant_pizza][:restaurant_id]
+      RestaurantPizza.create(pizza_id: @pizza.id, restaurant_id: restaurant_id)
+      redirect_to @pizza 
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_pizza)
+  end
 end
