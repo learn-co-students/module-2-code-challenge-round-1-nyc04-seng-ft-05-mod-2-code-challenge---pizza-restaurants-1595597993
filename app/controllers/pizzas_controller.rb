8767018d222a7560ce5393fdafbe7960e852429a
Zeynab-j <zeina2025@gmail.com>
2020-07-24 12:13:15 -0400

    done

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,8 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show 
+    @pizzas = Pizza.find(params[:id])
+  end
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,16 @@
 class Pizza < ApplicationRecord
+    has_many :restaurantpizzas
+    has_many :restaurants, through: :restaurantPizzas
+
+   validates: :name, presence: true 
+   validates: :ingredients, presence: true 
+
+    validates :name, uniqueness: true, presence: true 
+
+Pizzas should not have the same name.
+
+Add a validation to prevent this.
+
+Update the error handling and error display so that this error shows on the Pizza creation form.
+
 end
