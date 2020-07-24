a2847392196083a7f7e3fd788e496e2f6cf2b262
peawarrior <jacksonchen171@gmail.com>
2020-07-24 12:12:37 -0400

    DONE

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,28 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant
+  
   def index
-    @restaurants = Restaurant.all
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.assign_attributes(restaurant_params)
+    @restaurant.save ? (redirect_to @restaurant) : (render :edit)
+  end
+
+  private
+
+  def set_restaurant
+    params[:id].nil? ? (@restaurants = Restaurant.all) : (@restaurant = Restaurant.find(params[:id]))
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, pizza_ids: [])
+  end
+
 end
